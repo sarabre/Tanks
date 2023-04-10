e2f7620dd70c5d57adb32ca7aae24628c13f3758
DESKTOP-7C0JPBQ\Sara <sara>
2023-04-10 02:12:23 -0700

    Add a custom attribute to the RangedFloat struct so that their min or their max have the ability to not be 0 and 1.

---
+++ Assets/ScriptableObject/Audio/MinMaxRangeAttribute.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class MinMaxRangeAttribute : Attribute
+{
+   public MinMaxRangeAttribute(float min,float max)
+   {
+        Min = min;
+        Max = max;
+   }
+
+    public float Min { get; private set; }
+    public float Max { get; private set; }
+}
